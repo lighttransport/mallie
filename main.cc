c731aaa446de45de3d670d1c33ac30550f02d68b
Syoyo Fujita <syoyo@lighttransport.com>
2013-02-21 01:02:29 +0900

    Initial commit.

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstdlib>
+
+int
+main(
+  int argc,
+  char **argv)
+{
+  printf("[Mallie] Begin\n");
+  printf("[Mallie] End\n");
+  return EXIT_SUCCESS;
+}
+ 
---
+++ premake4.lua
@@ -0,0 +1,109 @@
+-- OpenMP
+newoption {
+   trigger     = "with-openmp",
+   description = "Use OpenMP."
+}
+
+-- SDL
+newoption {
+   trigger     = "with-sdl",
+   description = "Use SDL."
+}
+
+sources = {
+   "main.cc",
+   }
+
+newaction {
+   trigger     = "install",
+   description = "Install the software",
+   execute = function ()
+      -- copy files, etc. here
+   end
+}
+
+-- premake4.lua
+solution "MallieSolution"
+   configurations { "Release", "Debug" }
+
+   if (os.is("windows")) then
+      platforms { "native", "x32", "x64" }
+   else
+      platforms { "native", "x32", "x64" }
+   end
+
+   -- A project defines one build target
+   project "Mallie"
+      kind "ConsoleApp"
+      language "C++"
+      files { sources }
+
+      includedirs {
+         "./"
+      }
+
+      -- MacOSX. Guess we use gcc.
+      configuration { "macosx", "gmake" }
+
+         defines { '_LARGEFILE_SOURCE', '_FILE_OFFSET_BITS=64' }
+
+         -- SDL
+         if _OPTIONS['with-sdl'] then
+            defines { 'ENABLE_SDL' }
+            buildoptions { "`sdl-config --cflags`" }
+            buildoptions { "-msse2" }
+            linkoptions { "`sdl-config --libs`" }
+         end
+
+         -- gcc openmp
+         if _OPTIONS['with-openmp'] then
+            buildoptions { "-fopenmp" }
+            linkoptions { "-fopenmp" }
+         end
+
+      -- Windows specific
+      configuration { "windows", "gmake" }
+
+         defines { '__STDC_CONSTANT_MACROS', '__STDC_LIMIT_MACROS' } -- c99
+
+         links { "stdc++", "msvcrt", "ws2_32", "winmm" }
+
+      -- Windows specific
+      configuration { "windows", "vs2008", "x64" }
+
+         includedirs { "./compat" }
+
+         defines { 'NOMINMAX', '_LARGEFILE_SOURCE', '_FILE_OFFSET_BITS=64' }
+
+      configuration { "windows", "vs2008", "x32" }
+
+      	includedirs { "./compat" }
+
+         defines { 'NOMINMAX', '_LARGEFILE_SOURCE', '_FILE_OFFSET_BITS=64' }
+
+      -- Linux specific
+      configuration {"linux", "gmake"}
+         defines { '__STDC_CONSTANT_MACROS', '__STDC_LIMIT_MACROS' } -- c99
+
+         -- gcc openmp
+         if _OPTIONS['with-openmp'] then
+            buildoptions { "-fopenmp" }
+            linkoptions { "-fopenmp" }
+         end
+
+         linkoptions { "-pthread" }
+
+      -- Solaris specific
+      if (os.is("solaris")) then
+         -- todo
+      end
+
+      configuration "Debug"
+         defines { "DEBUG" } -- -DDEBUG
+         flags { "Symbols" }
+         targetname "marie_debug"
+
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Symbols", "Optimize", "EnableSSE2" }
+         targetname "marie"
