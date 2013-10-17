762302f7299191af02aecddd6d7e0f83ef2f7f91
Syoyo Fujita <syoyo@lighttransport.com>
2013-10-17 22:43:49 +0900

    Print MPI info.

--- main.cc
+++ main.cc
@@ -281,6 +281,7 @@ main(
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nnodes);
+  printf("[Mallie] MPI      : %d / %d\n", rank, nnodes);
 #endif
   printf("[Mallie] Version  : %s\n", MALLIE_VERSION);
 
