7c650f12400ee145fd97e38ff654db4929584f46
Syoyo Fujita <syoyo@lighttransport.com>
2013-10-17 22:47:19 +0900

    Initial PTex support.

--- texture.cc
+++ texture.cc
@@ -1,4 +1,34 @@
+#include <iostream>
+
 #ifdef ENABLE_PTEX
 #include <Ptexture.h>
 #endif
 
+#ifdef ENABLE_PTEX
+PtexCache*
+InitPtex()
+{
+  int maxMem = 1024*1024;
+  PtexCache* c = PtexCache::create(0, maxMem);
+
+  return c;
+}
+
+PtexTexture*
+LoadPtex(
+  PtexCache* cache,
+  const char* filename)
+{
+  Ptex::String err;
+  PtexTexture* r = PtexTexture::open(filename, err, /* premult */ 0);
+
+  printf("[Mallie] PtexTexture: %p\n", r);
+
+  if (!r) {
+    std::cerr << err.c_str() << std::endl;
+    return NULL;
+  }
+
+  return r;
+}
+#endif
